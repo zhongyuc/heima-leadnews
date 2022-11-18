638efefee80bc757f9985500f219dcf2b014cf46
zhongyucheng <1824788019@qq.com>
2022-11-18 15:33:17 +0800

    hot-fix commit1

--- heima-leadnews-common/src/main/java/com.athome/GitTest.java
+++ heima-leadnews-common/src/main/java/com.athome/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello4");
 
     }
 }
